e91b1a6add043dba3544b83db07cb90bb154b973
Muhammad Farhan <muhammad_farhan@outlook.co.id>
2021-01-09 21:59:34 +0700

    Menguji fungsi Enkripsi dan Dekripsi

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import encrypt
+import decrypt
 teks = 'Aku'
 encrypted = encrypt.Encrypt(teks, 5)
-print(encrypted)
+
+decrypted = decrypt.Decrypt(encrypted, 5)
+
+
+print(f"Teks awal adalah {teks}")
+print(f"Teks terenkripsi adalah {encrypted}")
+print(f"Hasil dekripsi dari Ciphertext adalah {decrypted}")
